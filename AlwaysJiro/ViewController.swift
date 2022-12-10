d6ead7a04f43aa195c5fbf2946f8bd9c382b866b
naokiwakata <rockwakata15@gmail.com>
2022-12-10 19:56:23 +0900

    Initial Commit

---
+++ AlwaysJiro/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AlwaysJiro
+//
+//  Created by 若田直希 on 2022/12/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
