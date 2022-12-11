24743fd2b8fbf8dfc541943aa9bc3a91e4eb7b57
naokiwakata <rockwakata15@gmail.com>
2022-12-11 23:05:10 +0900

    Firebaseと接続しFirestoreからusersコレクションを取得できることを確認

--- AlwaysJiro/controller/ViewController.swift
+++ AlwaysJiro/controller/ViewController.swift
@@ -6,15 +6,30 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UITabBarController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // タブバーの色変更
         UITabBar.appearance().tintColor = UIColor.orange
+        
+        // Firestoreを初期化する
+        let db = Firestore.firestore()
+        
+        // usersコレクションを取得する
+        db.collection("users").getDocuments() { (querySnapshot, err) in
+            if let err = err {
+                print("Error getting documents: \(err)")
+            } else {
+                for document in querySnapshot!.documents {
+                    print("\(document.documentID) => \(document.data())")
+                }
+            }
+        }
     }
-
-
+    
+    
 }
 
