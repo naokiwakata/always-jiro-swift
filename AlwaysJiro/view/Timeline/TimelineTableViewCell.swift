8209d4aaac930280c223bace530d8d3df7aa418e
naokiwakata <rockwakata15@gmail.com>
2022-12-13 15:12:47 +0900

    日付表示成功

--- AlwaysJiro/view/Timeline/TimelineTableViewCell.swift
+++ AlwaysJiro/view/Timeline/TimelineTableViewCell.swift
@@ -9,25 +9,32 @@ import UIKit
 import Nuke
 
 class TimelineTableViewCell: UITableViewCell {
-
+    
     @IBOutlet weak var name: UILabel!
     @IBOutlet weak var userImage: UIImageView!
+    @IBOutlet weak var publishedAt: UILabel!
     
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
-
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
+        
         // Configure the view for the selected state
+        
+        // 角丸にする
+        userImage.layer.cornerRadius = userImage.frame.size.width * 0.5
+        userImage.clipsToBounds = true
     }
     
     func setCell(user: User){
         self.name.text = user.displayName
+        let date: Date = user.createdAt.dateValue()
+        self.publishedAt.text = date.description
         let url = URL(string:  user.photoURL) ?? URL(string: "https://ogre.natalie.mu/artist/100506/20220303/konnoayaka_art202203.jpg?imwidth=640&imdensity=1")!
-
+        
         Nuke.loadImage(with: url , into: self.userImage)
     }
     
