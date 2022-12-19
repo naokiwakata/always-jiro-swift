aedd83fad283047dcfce867cb193ae562aa33ecd
naokiwakata <rockwakata15@gmail.com>
2022-12-19 23:37:08 +0900

    PhotoViewに写真を表示

--- AlwaysJiro/controller/PhotoViewController.swift
+++ AlwaysJiro/controller/PhotoViewController.swift
@@ -1,27 +1,68 @@
-//
-//  PhotoViewController.swift
-//  AlwaysJiro
-//
-//  Created by 若田直希 on 2022/12/19.
-//
-
 import UIKit
 
 class PhotoViewController: UIViewController {
-
-    @IBOutlet weak var label: UILabel!
+    
+    private var scrollView: UIScrollView!
+    private var pageControl: UIPageControl!
     
     var imageURLs:Array<String> = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
         
-        label.text = "PhotoView"
+        
+        // scrollViewの画面表示サイズを指定
+        scrollView = UIScrollView(frame: CGRect(x: 0, y: 300, width: self.view.frame.size.width, height: 500))
+        // scrollViewのサイズを指定（幅は1メニューに表示するViewの幅×ページ数）
+        scrollView.contentSize = CGSize(width: self.view.frame.size.width*3, height: 500)
+        // scrollViewのデリゲートになる
+        scrollView.delegate = self
+        // メニュー単位のスクロールを可能にする
+        scrollView.isPagingEnabled = true
+        // 水平方向のスクロールインジケータを非表示にする
+        scrollView.showsHorizontalScrollIndicator = false
+        self.view.addSubview(scrollView)
+        
+        // scrollView上にUIImageViewをページ分追加する(今回は3ページ分)
+        let imageView1 = createImageView(x: 0, y: 0, width: self.view.frame.size.width, height: 500, image: "ayaka1.jpeg")
+        scrollView.addSubview(imageView1)
+        
+        let imageView2 = createImageView(x: self.view.frame.size.width, y: 0, width: self.view.frame.size.width, height: 500, image: "ayaka2.jpeg")
+        scrollView.addSubview(imageView2)
+        
+        let imageView3 = createImageView(x: self.view.frame.size.width*2, y: 0, width: self.view.frame.size.width, height: 500, image: "ayaka3.jpeg")
+        scrollView.addSubview(imageView3)
+        
+        // pageControlの表示位置とサイズの設定
+        pageControl = UIPageControl(frame: CGRect(x: 0, y: 670, width: self.view.frame.size.width, height: 30))
+        // pageControlのページ数を設定
+        pageControl.numberOfPages = 3
+        // pageControlのドットの色
+        pageControl.pageIndicatorTintColor = UIColor.lightGray
+        // pageControlの現在のページのドットの色
+        pageControl.currentPageIndicatorTintColor = UIColor.black
+        self.view.addSubview(pageControl)
     }
     
     @IBAction func onTap(_ sender: Any) {
         print(imageURLs)
         self.dismiss(animated: true)
+    }
+    
+    // UIImageViewを生成
+    func createImageView(x: CGFloat, y: CGFloat, width: CGFloat, height: CGFloat, image: String) -> UIImageView {
+        let imageView = UIImageView(frame: CGRect(x: x, y: y, width: width, height: height))
+        let image = UIImage(named:  image)
+        imageView.image = image
+        return imageView
+    }
+}
+
+// scrollViewのページ移動に合わせてpageControlの表示も移動させる
+extension PhotoViewController: UIScrollViewDelegate {
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        pageControl.currentPage = Int(scrollView.contentOffset.x / scrollView.frame.size.width)
     }
 }
